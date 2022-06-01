c543ce147c5b4f035e41f270782a4d66aad41894
jargalbat <jargalbat@gmail.com>
2022-06-01 18:41:09 +0800

    added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <div>Hi</div>
-      <Navbar />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <>
+            <div>Navbar</div>
+            <div className='navbar'>
+                <div className='navbar-container'>
+                    <Link to='/' className='navbar-logo'>
+                        TRAVEL <i className='fab fa-typo3'></i>
+                    </Link>
+                </div>
+
+            </div>
+        </>
+    )
 }
 
 export default Navbar
