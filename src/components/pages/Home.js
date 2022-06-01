dce75b6c8678398acadd59f6489cafcb42016c97
jargalbat <jargalbat@gmail.com>
2022-06-01 20:45:51 +0800

    changes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import '../../App.css';
+import Cards from '../Cards';
 import HeroSection from '../HeroSection';
+import Footer from '../Footer';
 
-export default function Home() {
-    return (
-        <HeroSection />
-    );
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
 }
+
+export default Home;
