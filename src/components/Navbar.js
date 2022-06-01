9d2de4bf5fd30d7de1bafe6d5ea8c0331550dc5d
jargalbat <jargalbat@gmail.com>
2022-06-01 19:52:07 +0800

    added buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from './Button';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
 
     return (
         <div className='navbar'>
@@ -30,7 +43,7 @@ function Navbar() {
                     <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>Sign Up</Link>
                 </li>
             </ul>
-
+            {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
     )
 }
